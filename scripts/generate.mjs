9cf57e22129b29dd73adb1670a640ea7efb81c6a
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-05-14 11:06:01 +0300

    feat(TelegramBot): ✨ Add Telegram Bot documentation; includes setup instructions, connection details, and usage examples

--- scripts/generate.mjs
+++ scripts/generate.mjs
@@ -72,6 +72,7 @@ const altered_names = {
   RePurpose: "Blackbird Repurpose",
   AdobeExperienceManager: "Adobe Experience Manager",
   Blackbird: "Blackbird Service API",
+  TelegramBot: "Telegram Bot"
 };
 
 const skip_repos = ["docs", "template-repo", "NotionOAuth", "LanguageWire", "CraftCMS"];
